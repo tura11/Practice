43055d6ba2bdf845b577f02be333f5a9b0675412
Tymek <tymek_uramek@wp.pl>
2026-03-24 13:52:06 +0100

    Many storage var in 1 slot

---
+++ EvmSlots/src/SlotStorageV2.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+
+contract SlotStorageV2 {
+    
+    //slot0
+    uint128 public a = 1;
+    uint64 public b = 2;
+    uint32 public c = 3;
+    uint32 public d = 4;
+
+
+    //slot1
+    address public addr;
+    uint64 public x;
+    uint32 public y;
+
+
+
+}
