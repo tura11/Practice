d98a6714a8a02dec9057f76943f9e8ff3ed424d4
Tymek <tymek_uramek@wp.pl>
2026-04-15 10:09:41 +0200

    UpgradebleContract

---
+++ UpgradableContracts/src/BoxV1.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+import {UUPSUpgradeable}from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+
+contract BoxV1 is  Initializable,UUPSUpgradeable, OwnOwnableUpgradeableable {
+    uint256 internal number;
+
+    constructor(){
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
+
+
+
+    function getNumber() external view returns(uint256){
+        return number;
+    }
+
+
+    function version() external pure returns(uint256){
+        return 1;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal onlyOwner {
+        upgradeTo(newImplementation);
+    }
+}
