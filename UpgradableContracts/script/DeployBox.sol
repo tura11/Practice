f515abf1c63741519a10dd2d992b8e96574f7221
Tymek <tymek_uramek@wp.pl>
2026-04-17 15:10:50 +0200

    Feat: Script for upgrade contract

---
+++ UpgradableContracts/script/DeployBox.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+
+import {Script} from "forge-std/Script.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {BoxV1} from "../src/BoxV1.sol";
+
+
+contract DeployBox is Script {
+
+
+    function run() external returns(address){
+        address proxy = deployBox();
+        return proxy;
+    }
+
+
+    function deployBox() public returns(address){
+        vm.startBroadcast();
+        BoxV1 boxV1 = new BoxV1();
+        ERC1967Proxy proxy = ERC1967Proxy(address(boxV1), "");
+        vm.stopBroadcast();
+        return address(proxy);
+    }
+}
