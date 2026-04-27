8fdfe0044eba346dc57a8b84735a4d92f96cd1d9
Tymek <tymek_uramek@wp.pl>
2026-04-27 19:18:47 +0200

    FEAT: approve, allowance

--- ERC20LotterVote/src/VaultToken.sol
+++ ERC20LotterVote/src/VaultToken.sol
@@ -3,6 +3,11 @@
 pragma solidity 0.8.31;
 
 contract VaultToken {
+
+    error VaultToken__AddressZero(address adderess);
+    error VaultToken__CannotAllowanceToYourself();
+    error VaultToken__InvalidAllowance();
+
     string private symbol;
     string private name;
     uint8 private decimals;
@@ -10,7 +15,10 @@ contract VaultToken {
     uint256 totalSupply;
 
     mapping(address => uint256) private balances;
-    mapping(address => mapping(address => bool)) private allowances;
+    mapping(address => mapping(address => uint256)) private allowances;
+
+    event Minted(owner, amount);
+    event Burned(owner, amount);
 
     constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
         name = _name;
@@ -18,12 +26,52 @@ contract VaultToken {
         decimals = 18;
         initialSupply = _initialSupply;
     }
+    function mint(uint256 amount) external {
+        balances[msg.sender] += amount;
+        totalSupply += amount;
+        emit Minted(msg.sender, amount);
+    }
 
-    // function totalSupply() public view returns (uint256)
-    // function transfer(address _to, uint256 _value) public returns (bool success)
-    // function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
-    // function approve(address _spender, uint256 _value) public returns (bool success)
-    // function allowance(address _owner, address _spender) public view returns (uint256 remaining)
+
+    function burn(uint256 amount) external {
+        balances[msg.sender] -= amount;
+        totalSupply -= asmount;
+        emit Burned(msg.sender, amount);
+    }
+
+    function allowance(address owner, address spender) public view returns(uint256) {
+        if (owner == address(0)){
+            revert VaultToken__AddressZero(owner);
+        }
+
+        if (spender == address(0)){
+            revert VaultToken__AddressZero(spender);
+        }
+
+        if (spender == msg.sender){
+            revert VaultToken__CannotAllowanceToYourself();
+        }
+
+        return allowances[owner][spender];
+    }
+
+
+    function approve(address spender, uint256 value) public view returns(bool){
+        if(spender == address(0)){
+            revert VaultToken__AddressZero(spender);
+        }
+        allowances[msg.sender][spender] = value;
+        return true;
+    }
+
+
+    function transfer(address to, uint256 amount) public view returns(bool){
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+
+        (bool success,) = payable(to).call{value: amount}("");
+        return success;
+    }
 
     function getName() public view returns (string memory) {
         return name;
@@ -45,3 +93,5 @@ contract VaultToken {
         return totalSupply;
     }
 }
+    // function transfer(address _to, uint256 _value) public returns (bool success)
+    // function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
