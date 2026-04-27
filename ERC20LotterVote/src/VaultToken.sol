d68573681130affdee68ff8d139555894143c089
Tymek <tymek_uramek@wp.pl>
2026-04-27 19:28:34 +0200

    ERC20 full

--- ERC20LotterVote/src/VaultToken.sol
+++ ERC20LotterVote/src/VaultToken.sol
@@ -1,22 +1,21 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.31;
 
 contract VaultToken {
-
-    error VaultToken__AddressZero(address adderess);
-    error VaultToken__CannotAllowanceToYourself();
-    error VaultToken__InvalidAllowance();
+    error VaultToken__AddressZero(address addr);
+    error VaultToken__InsufficientBalance();
+    error VaultToken__InsufficientAllowance();
 
     string private symbol;
     string private name;
     uint8 private decimals;
-    uint256 private initialSupply;
-    uint256 totalSupply;
+    uint256 public totalSupply;
 
     mapping(address => uint256) private balances;
     mapping(address => mapping(address => uint256)) private allowances;
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
     event Minted(address indexed owner, uint256 amount);
     event Burned(address indexed owner, uint256 amount);
 
@@ -24,81 +23,56 @@ contract VaultToken {
         name = _name;
         symbol = _symbol;
         decimals = 18;
-        initialSupply = _initialSupply;
+        totalSupply = _initialSupply;
+        balances[msg.sender] = _initialSupply;
     }
+
     function mint(uint256 amount) external {
         balances[msg.sender] += amount;
         totalSupply += amount;
         emit Minted(msg.sender, amount);
     }
 
-
     function burn(uint256 amount) external {
+        if (balances[msg.sender] < amount) revert VaultToken__InsufficientBalance();
         balances[msg.sender] -= amount;
         totalSupply -= amount;
         emit Burned(msg.sender, amount);
     }
 
-    function allowance(address owner, address spender) public  returns(uint256) {
-        if (owner == address(0)){
-            revert VaultToken__AddressZero(owner);
-        }
+    function approve(address spender, uint256 value) public returns (bool) {
+        if (spender == address(0)) revert VaultToken__AddressZero(spender);
+        allowances[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
 
-        if (spender == address(0)){
-            revert VaultToken__AddressZero(spender);
-        }
-
-        if (spender == msg.sender){
-            revert VaultToken__CannotAllowanceToYourself();
-        }
-
+    function allowance(address owner, address spender) public view returns (uint256) {
         return allowances[owner][spender];
     }
 
-
-    function approve(address spender, uint256 value) public returns(bool){
-        if(spender == address(0)){
-            revert VaultToken__AddressZero(spender);
-        }
-        allowances[msg.sender][spender] = value;
-        return true;
-    }
-
-
-    function transfer(address to, uint256 amount) public  returns(bool){
+    function transfer(address to, uint256 amount) public returns (bool) {
+        if (to == address(0)) revert VaultToken__AddressZero(to);
+        if (balances[msg.sender] < amount) revert VaultToken__InsufficientBalance();
         balances[msg.sender] -= amount;
         balances[to] += amount;
-
+        emit Transfer(msg.sender, to, amount);
         return true;
     }
 
-    function transferFrom(address from, address to, uint256 amount) public  returns(bool){
+    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
+        if (to == address(0)) revert VaultToken__AddressZero(to);
+        if (balances[from] < amount) revert VaultToken__InsufficientBalance();
+        if (allowances[from][msg.sender] < amount) revert VaultToken__InsufficientAllowance();
+        allowances[from][msg.sender] -= amount;
         balances[from] -= amount;
         balances[to] += amount;
-        allowances[from][to] -= amount;
-        
+        emit Transfer(from, to, amount);
         return true;
-
     }
 
-    function getName() public view returns (string memory) {
-        return name;
-    }
-
-    function getSymbol() public view returns (string memory) {
-        return symbol;
-    }
-
-    function getDecimals() public view returns (uint8) {
-        return decimals;
-    }
-
-    function balanceOf(address owner) public view returns (uint256 balance) {
-        return balances[owner];
-    }
-
-    function getTotalSupply() public view returns (uint256) {
-        return totalSupply;
-    }
+    function balanceOf(address owner) public view returns (uint256) { return balances[owner]; }
+    function getName() public view returns (string memory) { return name; }
+    function getSymbol() public view returns (string memory) { return symbol; }
+    function getDecimals() public view returns (uint8) { return decimals; }
 }
-
