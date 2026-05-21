104816c5645b5f5a57ef023e3e9aaa5e3425aac5
Tymek <tymek_uramek@wp.pl>
2026-05-21 20:11:03 +0200

    Imports and refactor

--- ERC20LotterVote/src/Lottery.sol
+++ ERC20LotterVote/src/Lottery.sol
@@ -2,33 +2,20 @@
 
 pragma solidity 0.8.31;
 
-import "https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
-
-import {Vault} from "./Vault.sol";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
 
 contract Lottery {
     error Lottery__InvalidAddress();
     error Lottery__PlayerAlreadyEntered();
     error Lottery__EntryFeeToLow();
 
-    struct requestStatus {
-        bool fullfilled;
-        bool exist;
-        uint256[] randomWords;
-    }
-    VRFCoordinatorV2Interface COORDINATOR; //vrf coordinator
-    uint64 s_subscriptionId;
-    uint256[] public requestsIds;
-    uint256 public lastRequestId;
-    bytes32 immutable keyHash;
-    address public immutable linkToken;
 
     uint256 public constant ENTRY_FEE = 10e18; // 10 tokens
     address[] private players;
     mapping(address => bool) public isEntered;
-    mapping(uint256 => RequestStatus) public s_requests;
+
 
     function enterLottery(address player, uint256 enterFee) public {
         if (player == address(0)) {
@@ -46,5 +33,5 @@ contract Lottery {
 
     //todo vrf chanilik implenetanion
 
-    function pickRandomWinner() {}
+    function pickRandomWinner() public {}
 }
